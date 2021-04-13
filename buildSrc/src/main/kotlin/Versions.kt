05b032e4253a26903a8f91f3175c02db503cbd49
Hadi <hadilashkari@gmail.com>
2021-04-13 19:44:34 -0400

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,16 +5,14 @@ object Versions {
 
     const val kotlin_version = "1.4.32"
     const val dokka_version = "1.4.30"
-    const val android_maven_version = "2.1"
-    const val gradle_bintray_version = "1.8.4"
 
     const val groupId = "com.github.hadilq"
     const val artifactId = "live-event"
     const val libVersion = "1.2.1"
 
-    const val androidx_lib_version = "1.0.1"
-    const val constraint_layout_version = "1.1.3"
-    const val androidx_lifecycle_lib_version = "2.1.0"
+    const val androidx_lib_version = "1.2.0"
+    const val constraint_layout_version = "2.0.4"
+    const val androidx_lifecycle_lib_version = "2.2.0"
     const val junit_version = "4.12"
     const val androidx_testing = "2.1.0"
     const val mockito_version = "2.2.0"
